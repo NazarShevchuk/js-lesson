97682658589e44354ff0833e8b756d645d07f92c
NazarShevchuk <sevcuk031@gmail.com>
2022-11-28 21:55:22 +0200

    randomjs

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const cols = document.querySelectorAll(".col");
 
+document.addEventListener("keydown", (event) => {
+  if (event.code.toLowerCase() === "space") {
+    setRandomColors();
+  }
+});
+
 function gerenerateRandomColor() {
   // RGB
   // #FF0000
@@ -16,8 +22,22 @@ function gerenerateRandomColor() {
 
 function setRandomColors() {
   cols.forEach((col) => {
-    col.style.background = gerenerateRandomColor();
+    const text = col.querySelector("h2");
+    const button = col.querySelector("button");
+
+    const color = chroma.random();
+
+    text.textContent = color;
+    col.style.background = color;
+
+    setTextColor(text, color);
+    setTextColor(button, color);
   });
+}
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? "black" : "white";
 }
 
 setRandomColors();
